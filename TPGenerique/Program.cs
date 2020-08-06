fa0d0afaeca01dbae4cac97c4a36381ce880f948
franckyp1 <franckyp1+rapports_git>
2020-08-06 21:48:23 +0200

    Final

--- TPGenerique/Program.cs
+++ TPGenerique/Program.cs
@@ -6,13 +6,28 @@ namespace TPGenerique
     {
         static void Main(string[] args)
         {
-            ListeChainee<int> list = new ListeChainee<int>();
-
-            list.Ajouter(5);
-            list.Ajouter(10);
-            list.Ajouter(4);
-            Console.WriteLine(list.Premier.Valeur);
-            Console.WriteLine(list.Premier.Suivant.Valeur);
+            ListeChainee<int> listeChainee = new ListeChainee<int>();
+          
+            listeChainee.Ajouter(5);
+            listeChainee.Ajouter(10);
+            listeChainee.Ajouter(4);
+            Console.WriteLine(listeChainee.Premier.Valeur);
+            Console.WriteLine(listeChainee.Premier.Suivant.Valeur);
+            Console.WriteLine(listeChainee.Premier.Suivant.Suivant.Valeur);
+            Console.WriteLine("*************");
+            Console.WriteLine(listeChainee.ObtenirElement(0).Valeur);
+            Console.WriteLine(listeChainee.ObtenirElement(1).Valeur);
+            Console.WriteLine(listeChainee.ObtenirElement(2).Valeur);
+            Console.WriteLine("*************");
+            listeChainee.Inserer(99, 0);
+            listeChainee.Inserer(33, 2);
+            listeChainee.Inserer(30, 2);
+            Console.WriteLine(listeChainee.ObtenirElement(0).Valeur);
+            Console.WriteLine(listeChainee.ObtenirElement(1).Valeur);
+            Console.WriteLine(listeChainee.ObtenirElement(2).Valeur);
+            Console.WriteLine(listeChainee.ObtenirElement(3).Valeur);
+            Console.WriteLine(listeChainee.ObtenirElement(4).Valeur);
+            Console.WriteLine(listeChainee.ObtenirElement(5).Valeur);
         }
     }
 }
